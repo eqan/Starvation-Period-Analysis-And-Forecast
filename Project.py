f18d2bdd5b0e3b675475078d73e71ead6f1d236d
eqan <eqan.ahmad123@gmail.com>
2022-01-01 19:27:08 +0500

    Adding basic application with prediction ability

--- Project.py
+++ Project.py
@@ -3,30 +3,26 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(r'./Project-Dataset.csv', skiprows=range(1,55))
+import sys
 
-n=len(data)
-r = np.arange(n)
-width = 0.25
-  
-plt.bar(r, (data["Wheat Consumption Growth Rate"] + data["Barley Consumption Growth Rate"] + data["Meat Consumption Growth Rate"] + data["Rice Consumption Growth Rate"] )/4, color = 'b',
-        width = width, edgecolor = 'black',
-        label='Production')
+# 1. Import `QApplication` and all the required widgets
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
 
-plt.bar(r+width, (data["Wheat Production Growth Rate"] + data["Barley Production Growth Rate"] + data["Meat Production Growth Rate"] + data["Rice Production Growth Rate"] )/4, color = 'g',
-        width = width, edgecolor = 'black',
-        label='Consumption')
+# 2. Create an instance of QApplication
+app = QApplication(sys.argv)
 
-plt.bar(r+width*2, data["Hunger Annual Change"], color = 'y',
-        width = width, edgecolor = 'black',
-        label='Hunger')
+# 3. Create an instance of your application's GUI
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
+helloMsg.move(60, 15)
 
+# 4. Show your application's GUI
+window.show()
 
-plt.xlabel("Year")
-plt.ylabel("Annual Rate")
-plt.title("Comparison of Consumption & Production of Wheat with Hunger Growth")
-  
-plt.xticks(r + width/2, data["Year"])
-plt.legend()
-  
-plt.show()
+# 5. Run your application's event loop (or main loop)
+sys.exit(app.exec_())
